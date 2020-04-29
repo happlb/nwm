1c6cd4e77124864533f823fce211aa7fed00ba66
Lucy <happlb@msoe.edu>
2020-04-28 22:39:55 -0500

    adding in vue

---
+++ js/main.js
@@ -0,0 +1,9 @@
+var app = new Vue({
+    el: '#app',
+    data: {
+        country: 'Vietnam',
+        dish: 'Authentic Pho',
+        recipe: 'https://www.allrecipes.com/recipe/228443/authentic-pho/'
+
+    }
+})
